9b8a9756cedba31da7c133461e0c34c97d5cd72e
dimitrijjedich <dimitrij@startup-werk.de>
2025-01-26 20:14:00 +0100

    php artisan breeze:install
    Which Breeze stack would you like to install? -> React with Inertia
    
    Would you like any optional features? -> Dark mode + TypeScript
    
    Which testing framework do you prefer? -> Pest

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+            AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
         $middleware->web(append: [
             HandleInertiaRequests::class
         ]);
